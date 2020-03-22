cf9a3e36a4dc428c881840d9286c41715dfbea63
JuzouSatoru2 <mccr4fthd@gmail.com>
2020-03-22 16:31:14 +0100

    removing unused

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const messageController = require('../controller/message');
 
-router.get('/test', (req, res) => {
+router.get('/', (req, res) => {
     res.json('Api routes working');
 });
 
