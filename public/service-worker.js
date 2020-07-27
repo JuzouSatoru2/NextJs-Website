cd74bd0b90162a6b7bd3c74d5c9e023a5e9a3ae3
JuzouSatoru2 <mccr4fthd@gmail.com>
2020-07-27 22:29:11 +0200

    Update PWA accessibility

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,50 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 const { registerRoute } = workbox.routing;
-const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
+const { NetworkFirst, CacheFirst, StaleWhileRevalidate } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 self.__WB_DISABLE_DEV_LOGS = true;
 
 registerRoute(
-  /\.(?:css|js|otf)$/,
+  '/',
+  new NetworkFirst ({
+    cacheName: 'start-url',
+    expiration: {
+      maxEntries: 2,
+      maxAgeSeconds: 24 * 60 * 60
+    }
+  })
+);
+
+registerRoute(
+  /\.(?:js)$/,
   new StaleWhileRevalidate({
-    cacheName: 'main-cache',
+    cacheName: 'js-cache',
+    expiration: {
+      maxEntries: 32,
+      maxAgeSeconds: 24 * 60 * 60
+    }
+  })
+);
+
+registerRoute(
+  /\.(?:css)$/,
+  new StaleWhileRevalidate({
+    cacheName: 'style-cache',
+    expiration: {
+      maxEntries: 32,
+      maxAgeSeconds: 24 * 60 * 60
+    }
+  })
+);
+
+registerRoute(
+  /\.(?:otf)$/,
+  new StaleWhileRevalidate({
+    cacheName: 'font-cache',
+    expiration: {
+      maxEntries: 4,
+      maxAgeSeconds: 7 * 24 * 60 * 60
+    }
   })
 );
 
@@ -17,7 +54,7 @@ registerRoute(
     cacheName: 'image-cache',
     plugins: [
       new ExpirationPlugin({
-        maxEntries: 20,
+        maxEntries: 64,
         maxAgeSeconds: 7 * 24 * 60 * 60,
       })
     ],
