a1af2a992c4dad9a0ff445a5372a99cc07ca7d39
JuzouSatoru2 <mccr4fthd@gmail.com>
2020-08-29 15:17:33 +0200

    Remove wrong executing tests

--- cypress/integration/api_spec.js
+++ cypress/integration/api_spec.js
@@ -4,17 +4,4 @@ describe("Test api routes", () => {
       expect(response.status).to.eq(200);
     });
   });
-  it("Checks auth of /api/msg", () => {
-    cy.request("/api/msg", { failOnStatusCode: false }).then((response) => {
-      expect(response.status).to.eq(403);
-    });
-    cy.request("/api/msg/value", { failOnStatusCode: false }).then((response) => {
-      expect(response.status).to.eq(403);
-    });
-  });
-  it("Checks auth for /monitor", () => {
-    cy.request("/api/monitor", { failOnStatusCode: false }).then((response) => {
-      expect(response.status).to.eq(403);
-    });
-  });
 });
