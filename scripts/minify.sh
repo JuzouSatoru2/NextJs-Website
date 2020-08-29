e9ac420519dd6d3d7321728e7935afe07dbd3294
JuzouSatoru2 <mccr4fthd@gmail.com>
2020-08-29 16:13:41 +0200

    Remove unused styles

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd ./public/static/css
-node-minify --compressor clean-css --input nav.css --output nav.min.css && node-minify --compressor clean-css --input style.css --output style.min.css && cd ../js && node-minify --compressor uglify-es --input main.js --output main.min.js && node-minify --compressor uglify-es --input nav.js --output nav.min.js && cd ../../../ && echo Everything Done!
+node-minify --compressor clean-css --input nav.css --output nav.min.css && cd ../js && node-minify --compressor uglify-es --input main.js --output main.min.js && node-minify --compressor uglify-es --input nav.js --output nav.min.js && cd ../../../ && echo Everything Done!
